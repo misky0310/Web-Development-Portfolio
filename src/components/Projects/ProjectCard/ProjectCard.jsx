be4ce7b3d5627ceb3cdb25753df6ac2a7439c858
Miskat <misky0310@gmail.com>
2024-04-18 16:36:28 +0530

    Improved Projects Section

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
+import Reveal from '../../Reveal/Reveal';
+import { IoLogoGithub } from "react-icons/io5";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="max-w-screen-lg mx-auto mt-8 p-4 border border-gray-300 rounded-lg shadow-md">
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-2/3">
-          <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-          <p className="text-gray-600 mb-4">{project.description}</p>
-          <div className="flex justify-between">
-            <div>
-              <h3 className="text-lg font-semibold">Technologies Used:</h3>
-              <ul className="list-disc pl-6">
-                {project.technologies.map((tech, index) => (
-                  <li key={index} className="text-gray-600">{tech}</li>
-                ))}
-              </ul>
+    <Reveal>
+        <div className="max-w-screen-lg mx-auto mt-8 p-4 border border-gray-300 rounded-lg shadow-md">
+            <div className="flex flex-col md:flex-row">
+                <div className="">
+                <h2 className="text-xl font-semibold mb-2 text-left">{project.title}</h2>
+                <p className="text-gray-600 mb-4 h-[30rem]">{project.description}</p>
+                    <div className='h-[13rem] '>
+                    <h3 className="text-lg font-semibold">Technologies Used:</h3>
+                    <ul className="list-disc pl-6">
+                        {project.technologies.map((tech, index) => (
+                        <li key={index} className="text-gray-600">{tech}</li>
+                        ))}
+                    </ul>
+                    </div>
+                    <a href={`${project.link}`} className='flex items-center justify-center gap-2'><IoLogoGithub className='h-6 w-6'/>Click here to visit the github page</a>
+                </div>
             </div>
-            <div>
-              <h3 className="text-lg font-semibold">Year:</h3>
-              <p className="text-gray-600">{project.year}</p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
+    </Reveal>
   );
 };
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,44 +1,45 @@
 import React from 'react'
 import ProjectCard from './ProjectCard/ProjectCard';
+import Reveal from '../Reveal/Reveal';
 
 
 const projects = [
   {
-    title: "Project 1",
-    description: "Description of Project 1",
-    image: "project1.jpg",
-    technologies: ["React", "Node.js", "Express"],
-    year: 2022
+    title: "BeyoundBoundsBlog",
+    description: "A responsive blog application built with React.js using the Vite framework, state management with User Context, and styled with Tailwind CSS. The application allows users to interact with other people by providing a platform where they can share the latest updates happening around the world on different issues.Users can easily signup to the blog website. The data is stored locally in a DB(MongoDB),provide correct credentials and easily log in to the profile , easily add a new post sharing thoughts on the latest happenings,make changes if necessary after the post has been added. Bcrypt is used to encrypt the password so as to achieve basic security measures.The application is designed to work seamlessly on various screen sizes, including desktops, tablets, and mobile devices.",
+    technologies: ["React.js", "Tailwind", "Node.js","Express","MongoDB","Bcrypt.js","JavaScript"],
+    link:'https://github.com/misky0310/BeyondBoundsBlog'
   },
   {
-    title: "Project 2",
-    description: "Description of Project 2",
-    image: "project2.jpg",
-    technologies: ["Angular", "Java", "Spring Boot"],
-    year: 2021
+    title: "ToDo List App",
+    description: "A responsive todo list application built with React.js using the Vite framework, state management with Redux Toolkit, and styled with Tailwind CSS. The application allows users to efficiently manage their tasks by providing a clean and intuitive user interface.Users can easily add new tasks to the todo list, mark tasks as completed to keep track of the progress, remove tasks that are no longer needed or have been completed.The application is designed to work seamlessly on various screen sizes, including desktops, tablets, and mobile devices.",
+    technologies: ["React.js", "Tailwind", "React Redux", "JavaScript"],
+    link:'https://github.com/misky0310/ToDoList-Redux'
   },
   {
-    title: "Project 2",
-    description: "Description of Project 2",
-    image: "project2.jpg",
-    technologies: ["Angular", "Java", "Spring Boot"],
-    year: 2021
+    title: "True Feedback",
+    description: "True Feedback is a web application designed for honest and anonymous communication among users. Built with Next.js, it leverages the power of server-side rendering and efficient routing for seamless user experience. NextAuth.js is integrated for authentication, providing a robust and secure authentication solution out of the box.The platform allows users to send anonymous messages to each other, fostering open and genuine dialogue. Users can sign up easily to access the platform's features. Data is stored locally in a MongoDB database. Users can securely log in to their profiles with encrypted passwords using Bcrypt.They can send and receive anonymous messages, as well as initiate direct messages by providing user IDs or profiles. The application is designed to be responsive, ensuring seamless functionality across desktops, tablets, and mobile devices.",
+    technologies: ["Next.js", "Next-Auth", "MongoDB","Bcrypt","Tailwind CSS","Zod","React Hook Form"],
+    link:'https://github.com/misky0310/TrueFeedback'
   },
   // Add more project objects as needed
 ];
 
 const Projects = () => {
   return (
-    <div id="projects" className="projects w-full">
-        <div className="textCont flex flex-col items-center justify-center text-center py-20 gap-8 w-full border">
-            <h1 className='text-6xl'>My Clients</h1>
+    <Reveal>
+      <div id="projects" className="projects w-full">
+        <div className="textCont flex flex-col items-center justify-center text-center py-20 gap-8 w-full">
+            <h1 className='text-6xl'>My Projects</h1>
         </div>
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 gap-5'>
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
         </div>    
-    </div>
+      </div>
+
+    </Reveal>
   )
 }
 
